9e7047d1a7ef501b244c3844d0f6ff28d5d227dc
hasnainhawlader <hasnain.hawlader@gmail.com>
2023-03-11 23:36:17 +0600

    find a user

--- controllers/userController.js
+++ controllers/userController.js
@@ -108,16 +108,4 @@ const findUser = async (req, res) => {
   }
 };
 
-// get all users
-const getAllUsers = async (req, res) => {
-  try {
-    const users = await userModel.find({});
-
-    res.status(200).json(users);
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
-};
-
 module.exports = { registerUser, loginUser, findUser, getAllUsers };
